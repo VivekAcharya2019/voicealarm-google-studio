c3c394af1bcd30ce1e0d71a392fc98540845e2e6
Vivek.Acharya <vivek.acharya@navex.com>
2026-03-30 22:24:16 +0530

    Added complete app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vivekacharya.smartvoicealarm',
+  appName: 'smartvoicealarm',
+  webDir: 'dist'
+};
+
+export default config;
